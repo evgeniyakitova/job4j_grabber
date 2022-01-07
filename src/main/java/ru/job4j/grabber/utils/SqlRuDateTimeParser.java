c245d64093e68bd9996d2ee0b34beec93c96199a
evgeniyakitova <kitova.evgeniya.00@mail.ru>
2022-01-07 18:02:38 +0300

    2.1. Преобразование даты [#289476]

---
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -0,0 +1,42 @@
+package ru.job4j.grabber.utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Map;
+
+public class SqlRuDateTimeParser implements DateTimeParser {
+    private static final Map<String, String> MONTHS = Map.ofEntries(
+            Map.entry("янв", "01"),
+            Map.entry("фев", "02"),
+            Map.entry("мар", "03"),
+            Map.entry("апр", "04"),
+            Map.entry("май", "05"),
+            Map.entry("июн", "06"),
+            Map.entry("июл", "07"),
+            Map.entry("авг", "08"),
+            Map.entry("сен", "09"),
+            Map.entry("окт", "10"),
+            Map.entry("ноя", "11"),
+            Map.entry("дек", "12"));
+
+    @Override
+    public LocalDateTime parse(String parse) {
+        if (parse.contains("сегодня")) {
+            LocalDate date = LocalDate.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MM yy");
+            parse = parse.replace("сегодня", date.format(formatter));
+        } else if (parse.contains("вчера")) {
+            LocalDate date = LocalDate.now().minusDays(1);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MM yy");
+            parse = parse.replace("вчера", date.format(formatter));
+        } else {
+        String month = parse.split(" ")[1];
+        parse = parse.replace(month, MONTHS.get(month));
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MM yy, HH:mm");
+        return LocalDateTime.parse(parse, formatter);
+    }
+}
