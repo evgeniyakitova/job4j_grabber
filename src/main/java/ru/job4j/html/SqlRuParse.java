adc3b6ee9c9167d25c991c57dc6786770de87045
evgeniyakitova <kitova.evgeniya.00@mail.ru>
2022-01-09 21:28:54 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,6 +5,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         for (int i = 1; i < 6; i++) {
@@ -13,10 +15,18 @@ public class SqlRuParse {
             Elements row = doc.select(".postslisttopic");
             for (Element td : row) {
                 Element href = td.child(0);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                System.out.println(td.lastElementSibling().text());
+                getDetails(href.attr("href"));
             }
         }
+    }
+
+    private static void getDetails(String link) throws IOException {
+        Document doc = Jsoup.connect(link).get();
+        Elements headers = doc.select(".messageHeader");
+        System.out.println(headers.first().ownText());
+        Elements bodies = doc.select(".msgBody");
+        System.out.println(bodies.get(1).text());
+        Elements footers = doc.select(".msgFooter");
+        System.out.println(footers.first().ownText().split("\\s\\[")[0]);
     }
 }
